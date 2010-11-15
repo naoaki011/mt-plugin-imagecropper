2da6dbe60bacc6f9e866e074fefc3c255e787c31
naoaki011 <naoaki@orion.ocn.ne.jp>
2010-11-15 19:00:36 +0900

    Except Theme binding for MT5.

--- plugins/ImageCropper/lib/ImageCropper/Plugin.pm
+++ plugins/ImageCropper/lib/ImageCropper/Plugin.pm
@@ -90,7 +90,8 @@ sub init_app {
 
 sub complete_upload_wrapper {
     my $app      = shift;
-    my $asset_id = $app->param('id');
+    my $q        = $app->can('query') ? $app->query : $app->param;
+    my $asset_id = $q->param('id');
 
     ###l4p $logger     ||= MT::Log->get_logger();  $logger->trace();
 
@@ -107,9 +108,9 @@ sub complete_upload_wrapper {
                     'from'        => 'view',
                     '_type'       => 'asset',
                     'id'          => $asset_id,
-                    'blog_id'     => $app->param('blog_id'),
+                    'blog_id'     => $q->param('blog_id'),
                     'return_args' => $app->return_args,
-                    'magic_token' => $app->param('magic_token')
+                    'magic_token' => $q->param('magic_token')
                 }
             )
         );
@@ -126,7 +127,8 @@ sub hdlr_default_text {
 sub del_prototype {
     my ($app) = @_;
     $app->validate_magic or return;
-    my @protos = $app->param('id');
+    my $q = $app->can('query') ? $app->query : $app->param;
+    my @protos = $q->param('id');
     for my $pid (@protos) {
         my $p = MT->model('thumbnail_prototype')->load($pid) or next;
         $p->remove;
@@ -184,7 +186,7 @@ sub _hdlr_pass_tokens_else {
 sub save_prototype {
     my $app = shift;
     my $param;
-    my $q   = $app->{query};
+    my $q = $app->can('query') ? $app->query : $app->param;
     my $obj = MT->model('thumbnail_prototype')->load( $q->param('id') )
       || MT->model('thumbnail_prototype')->new;
 
@@ -202,7 +204,7 @@ sub save_prototype {
 sub edit_prototype {
     my $app     = shift;
     my ($param) = @_;
-    my $q       = $app->{query};
+    my $q       = $app->can('query') ? $app->query : $app->param;
     my $blog    = MT::Blog->load( $q->param('blog_id') );
 
     $param ||= {};
@@ -261,7 +263,7 @@ sub load_ts_prototypes {
 sub list_prototypes {
     my $app = shift;
     my ($params) = @_ || {};
-    my $q        = $app->{query};
+    my $q = $app->can('query') ? $app->query : $app->param;
     my $blog     = $app->blog;
 
     if ( $blog && $app->blog->template_set ) {
@@ -401,7 +403,7 @@ sub gen_thumbnails_start {
 
 sub delete_crop {
     my $app  = shift;
-    my $q    = $app->param;
+    my $q    = $app->can('query') ? $app->query : $app->param;
     my $blog = $app->blog;
     my $id   = $q->param('id');
     my $key  = $q->param('prototype');
@@ -436,7 +438,7 @@ sub delete_crop {
 
 sub crop {
     my $app  = shift;
-    my $q    = $app->param;
+    my $q = $app->can('query') ? $app->query : $app->param;
     my $blog = $app->blog;
 
     my $X         = $q->param('x');
