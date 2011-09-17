74ddcc6374c627e7623ae3bf5df743c4083947c2
naoaki011 <naoaki@orion.ocn.ne.jp>
2011-09-17 20:53:30 +0900

    add partial translate.

---
+++ plugins/ImageCropper/lib/ImageCropper/L10N/en_us.pm
@@ -0,0 +1,11 @@
+package ImageCropper::L10N::en_us;
+
+use strict;
+
+use base 'ImageCropper::L10N';
+use vars qw( %Lexicon );
+%Lexicon = (
+
+);
+
+1;
---
+++ plugins/ImageCropper/lib/ImageCropper/L10N/ja.pm
@@ -0,0 +1,31 @@
+package ImageCropper::L10N::ja;
+
+use strict;
+use base 'AssetHandler::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+	'Allows users to define a set of thumbnail prototypes that others can easily generate using a drag-and-drop image cropping tool.'
+		=> 'ユーザーがサムネイルのひな型を作成でき、そのひな型をを使いドラッグドロップにより画像の切り取りが可能になります。',
+	'Thumbnail Prototype' => 'サムネイルのひな型',
+	'Thumbnail Prototypes' => 'サムネイルのひな型',
+	'Generate Thumbnails' => 'カスタムサムネイルを作成',
+	'Create Crop Prototype' => 'サムネイルのひな型を作成',
+	'Crop Prototype' => 'サムネイルのひな型',
+	'Crop Prototypes' => 'サムネイルのひな型',
+	'Prototypes' => 'ひな型',
+	'Max Width' => '最大幅',
+	'Max Height' => '最大高',
+	'Parent AssetID' => '親アイテムID',
+	'Cropped Text' => '注釈テキスト',
+	'Default Compress' => 'デフォルトの圧縮値',
+	'Annotation FontFamily' => '注釈のフォント',
+	'Annotation FontSize' => '注釈のフォントサイズ',
+
+
+);
+
+1;
+
