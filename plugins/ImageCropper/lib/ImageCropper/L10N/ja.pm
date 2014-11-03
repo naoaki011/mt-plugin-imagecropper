e779e97d058e7724e213c521d95c87cd2890ea19
Koji Matsumoto <libra804.sakura@gmail.com>
2014-11-03 23:06:24 +0900

    利用可能にする為の修正。
    MT6で利用できるようにする為の修正。

--- plugins/ImageCropper/lib/ImageCropper/L10N/ja.pm
+++ plugins/ImageCropper/lib/ImageCropper/L10N/ja.pm
@@ -1,7 +1,11 @@
 package ImageCropper::L10N::ja;
 
 use strict;
-use base 'AssetHandler::L10N::en_us';
+#>>ewm_kmatsumoto140629_1642
+#use base 'AssetHandler::L10N::en_us';
+#use base 'ImageCropper::L10N::en_us';
+use base 'ImageCropper::L10N::en_us';
+#<<ewm_kmatsumoto140629_1642
 use vars qw( %Lexicon );
 
 ## The following is the translation table.
@@ -10,7 +14,7 @@ use vars qw( %Lexicon );
 	'Allows users to define a set of thumbnail prototypes that others can easily generate using a drag-and-drop image cropping tool.'
 		=> 'ユーザーがサムネイルのひな型を作成でき、そのひな型をを使いドラッグドロップにより画像の切り取りが可能になります。',
 	'Thumbnail Prototype' => 'サムネイルのひな型',
-	'Thumbnail Prototypes' => 'サムネイルのひな型',
+	'Thumbnail Prototypes' => 'サムネイルのひな型の一覧',
 	'Generate Thumbnails' => 'カスタムサムネイルを作成',
 	'Create Crop Prototype' => 'サムネイルのひな型を作成',
 	'Crop Prototype' => 'サムネイルのひな型',
@@ -23,24 +27,27 @@ use vars qw( %Lexicon );
 	'Default Compress' => 'デフォルトの圧縮値',
 	'Annotation FontFamily' => '注釈のフォント',
 	'Annotation FontSize' => '注釈のフォントサイズ',
-	'A new Thumbnail Prototype has been successfully created.' => 'A new Thumbnail Prototype has been successfully created.',
-	'The selected protype(s) have been deleted.' => 'The selected protype(s) have been deleted.',
-	'Delete selected prototypes (d)' => 'Delete selected prototypes (d)',
-	'Custom Thumbnail Prototypes' => 'Custom Thumbnail Prototypes',
-	'Create Thumbnail Prototype' => 'Create Thumbnail Prototype',
-	'No custom thumbnail prototypes could be found.' => 'No custom thumbnail prototypes could be found.',
-	'Thumbnail Prototypes for [_1]' => 'Thumbnail Prototypes for [_1]',
-	'Create Thumbnail for [_1]' => 'Create Thumbnail for [_1]',
+	'A new Thumbnail Prototype has been successfully created.' => 'サムネイルのひな型は正常に作成されました。',
+	'The selected protype(s) have been deleted.' => 'サムネイルのひな型正常に削除されました。',
+	'Delete selected prototypes (d)' => '選択したサムネイルのひな型は削除されました。',
+	'Custom Thumbnail Prototypes' => 'サムネイルのひな型の編集',
+	'Create Thumbnail Prototype' => 'サムネイルのひな型の作成',
+	'No custom thumbnail prototypes could be found.' => 'サムネイルのひな型は作成されていません。',
+	'Thumbnail Prototypes for [_1]' => '[_1] のサムネイルのひな型',
+	'Create Thumbnail for [_1]' => '[_1]のサムネイルの作成',
 	'You have no thumbnail prototypes defined. Please <a href="[_1]">create some thumbnail prototypes</a>.'
-		=>'You have no thumbnail prototypes defined. Please <a href="[_1]">create some thumbnail prototypes</a>.',
-	'Return to the asset editing screen' => 'Return to the asset editing screen',
-	'Edit Prototypes' => 'Edit Prototypes',
-	'Select a Thumbnail Prototype' => 'Select a Thumbnail Prototype',
-	'Select your crop area' => 'Select your crop area',
-	'Select additional crop options' => 'Select additional crop options',
-	'Include Text Annotation?' => 'Include Text Annotation?',
-	'Text Annotation' => 'Text Annotation',
-	'Font Size' => 'Font Size',
+		=>'サムネイルのひな型が定義されていません。. <a href="[_1]">サムネイルのひな型の作成</a>を押してください。',
+	'Return to the asset editing screen' => 'アイテムの編集画面に戻る',
+	'Edit Prototypes' => 'サムネイルのひな型を作成する',
+	'Return to asset' => '「アイテムの編集」画面に戻る',
+	'Return to Listing' => '「アイテムの管理」画面に戻る',
+	'Return to the asset listing screen' => '「アイテムの管理」画面に戻る',
+	'Select a Thumbnail Prototype' => 'サムネイルのひな型を選択する',
+	'Select your crop area' => 'トリミングの範囲を選択する',
+	'Select additional crop options' => '必要に応じてトリミングの拡張設定を行う',
+	'Include Text Annotation?' => '画像の表示内ににテキストを埋め込む',
+	'Text Annotation' => 'テキストの埋め込み',
+	'Font Size' => 'フォントサイズ',
 	'Annotation Location' => '注釈の位置',
 	'Top Left' => '左上',
 	'Top Right' => '右上',
@@ -49,14 +56,14 @@ use vars qw( %Lexicon );
 	'Annotation Rotation' => '注釈の向き',
 	'Horizontal' => '水平',
 	'Vertical' => '垂直',
-	'Thumbnail Image Type' => 'Thumbnail Image Type',
-	'Thumbnail Quality' => 'Thumbnail Quality',
-	'Crop' => 'Crop',
-	'Edit Thumbnail Prototype' => 'Edit Thumbnail Prototype',
+	'Thumbnail Image Type' => '画像の保存形式',
+	'Thumbnail Quality' => '画像の品質',
+	'Crop' => 'トリミングする',
+	'Edit Thumbnail Prototype' => 'サムネイルのひな型の作成',
 	'Enter in a very short label that will be used to help you identify any thumbnail derived from this prototype.'
 		=> 'Enter in a very short label that will be used to help you identify any thumbnail derived from this prototype.',
-	'Enter in the max width of the thumbnail.' => 'Enter in the max width of the thumbnail.',
-	'Enter in the max height of the thumbnail.' => 'Enter in the max height of the thumbnail.',
+	'Enter in the max width of the thumbnail.' => 'サムネイルの最大幅を入力してください。',
+	'Enter in the max height of the thumbnail.' => 'サムネイルの最大縦幅を入力してください。',
 	
 );
 
